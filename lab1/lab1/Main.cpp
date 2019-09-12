90315a71f8257a8f8e16ee664aa9854069d06ef9
SerhiyTrots <55109509+SerhiyTrots@users.noreply.github.com>
2019-09-12 19:57:29 +0300

    Update Main.cpp
    3 methods and gist

--- lab1/lab1/Main.cpp
+++ lab1/lab1/Main.cpp
@@ -1,72 +1,74 @@
-#include <iostream>
+ #include <iostream>
+#include <ctime>
 using namespace std;
+ 
+unsigned int m = 2147483646;
 
-void method1()
+double method1(int x)
 {
-	unsigned int m = 2147483646;
 	int c = 5;
 	int a = 3;
-	int n = 1000;
-	int start = 0;
-	int x = 0;
-	while (start < n)
-	{
-		double u;
-		x = (a*x + c) % m;
-		u = double(x) / double(m);
-		cout << u << endl;
-		start++;
-	}
-
+	x = (a*x + c) % m;
+	return x;
 }
 
-void method2()
+double method2(int x)
 {
-	unsigned int m = 2147483646;
 	int c = 5;
 	int a = 3;
 	int d = 4;
-	int n = 1000;
-	int start = 0;
-	int x = 0;
-	while (start < n)
-	{
-		double u;
-		x = (d*x*x + a * x + c) % m;
-		u = double(x) / double(m);
-		cout << u << endl;
-		start++;
-	}
+	x = (d*x*x + a * x + c) % m;
+	return x;
 }
 
-void method3()
+double method3(int x, int x1, int x0)
 {
-	unsigned int m = 2147483646;
-	int n = 1000;
-	int start = 0;
-	int x1 = 1;
-	int x0 = 0;
-	int a;
-	while (start < n)
-	{
-		double u;
-		a = (x1 + x0) % m;
-		u = double(a) / double(m);
-		cout << u << endl;
-		x0 = x1;
-		x1 = a;
-		start++;
-	}
+	x = (x1 + x0) % m;
+	x0 = x1;
+	x1 = x;
+	return x;
+}
 
+void gist()
+{
+	const int n = 1000;
+	double arr[n];
+	arr[0] = time(NULL);
+	int number;
+	cin >> number;
+	for (int i = 1; i < n; i++)
+	{
+		switch (number)
+		{
+		case 1:
+			arr[i] = method1(arr[i - 1]);
+			break;
+		case 2:
+			arr[i] = method2(arr[i - 1]);
+			break;
+		case 3:
+			arr[i] = method3(arr[i - 1], arr[i - 2], arr[i - 3]);
+			break;
+		}
+	}
+	int count = 0;
+	for (double intr = 0.0; intr < 0.9; intr += 0.1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			double u = double(arr[i]) / double(m);
+			if ((u <= intr + 0.1) && (u >= intr))
+				count++;
+		}
+		cout << "[" << intr << " ; " << intr + 0.1 << "]" << "  " << count << endl;
+		count = 0;
+	}
 }
 
 int main()
 {
-	method1();
+	gist();
 
 	system("pause");
 	return 0;
 }
-
-
- 
