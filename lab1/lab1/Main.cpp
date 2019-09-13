b33da09a397122203f21d41eae4dad2cdb392cc2
SerhiyTrots <55109509+SerhiyTrots@users.noreply.github.com>
2019-09-13 17:10:04 +0300

    Update Main.cpp
    4 method

--- lab1/lab1/Main.cpp
+++ lab1/lab1/Main.cpp
@@ -30,6 +30,29 @@ double method3(int x, int x1, int x0)
 	return x;
 }
 
+double findReversal(double x)
+{
+	int p = 332423;
+	for (int i = 1; i < p; i++)
+	{
+		if ((int(x)*i) % p == 1)
+			return i;
+	}
+
+}
+
+double method4(int x)
+{
+	int p = 12421;
+	int a = 234;
+	int c = 1234;
+	unsigned int m = 2147483647;
+	double xr = findReversal(x);
+	x = int((a*xr + c)) % p;
+	return x;
+}
+
+
 double method5(double a, double b)
 {
 	unsigned int m = 2147483647;
@@ -80,6 +103,16 @@ void gist()
 			}
 			break;
 		}
+		case 4:
+		{
+			unsigned int m = 2147483647;
+			arr[i] = method4(arr[i - 1]);
+			for (int i = 0; i < n; i++)
+			{
+				arrS[i] = arr[i] / double(m);
+			}
+
+		}
 		case 5:
 		{
 			unsigned int m = 2147483647;
@@ -93,15 +126,30 @@ void gist()
 		}
 	}
 	int count = 0;
-	for (double intr = 0.0; intr < 0.9; intr += 0.1)
+	double begin;
+	double step;
+	double final;
+	if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+	{
+		begin = 0.0;
+		step = 0.1;
+		final = 0.9;
+	}
+	else
+	{
+		begin = -3.0;
+		step = 0.5;
+		final = 3.0;
+	}
+	for (double intr = begin; intr < final; intr += step)
 	{
 		for (int i = 0; i < n; i++)
 		{
 
-			if ((arrS[i] <= intr + 0.1) && (arrS[i] >= intr))
+			if ((arrS[i] <= intr + step) && (arrS[i] >= intr))
 				count++;
 		}
-		cout << "[" << intr << " ; " << intr + 0.1 << "]" << "  " << count << endl;
+		cout << "[" << intr << " ; " << intr + step << "]" << "  " << count << endl;
 		count = 0;
 	}
 }
