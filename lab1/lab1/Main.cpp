8f5bfd17cd527d557590120863fa6f5dd669808d
SerhiyTrots <55109509+SerhiyTrots@users.noreply.github.com>
2019-09-14 13:20:35 +0300

    Update Main.cpp
    6,7 and 8 methods

--- lab1/lab1/Main.cpp
+++ lab1/lab1/Main.cpp
@@ -1,5 +1,7 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 double method1(int x)
@@ -59,6 +61,55 @@ double method5(double a, double b)
 	return int((a - b)) % m;
 }
 
+double method6(unsigned int x1)
+{
+	unsigned int m = 2147483647;
+	unsigned int arr[12];
+	arr[0] = x1;
+	double sum = 0;
+	for (int i = 1; i < 12; i++)
+	{
+		arr[i] = method1(arr[i - 1]);
+		sum += double(arr[i]) / double(m);
+	}
+
+	return sum - 6;
+}
+double  method7(double a, double b, double * arr)
+{
+	unsigned int m = 2147483647;
+	double a1, b1, v1, v2, s;
+	do
+	{
+		a = method1(a);
+		b = method2(b);
+		a1 = a / double(m);
+		b1 = b / double(m);
+		v1 = 2 * a1 - 1;
+		v2 = 2 * b1 - 1;
+		s = v1 * v1 + v2 * v2;
+	} while (s >= 1);
+	double x1 = v1 * sqrt(((-2) * log(s) / log(M_E)) / s);
+	double x2 = v2 * sqrt(((-2) * log(s) / log(M_E)) / s);
+	arr[0] = x1;
+	arr[1] = x2;
+	return  arr[2];
+}
+
+double method8(double a, double b)
+{
+	unsigned int m = 2147483647;
+	double a1, b1, x;
+	do
+	{
+		a = method1(a);
+		b = method2(b);
+		a1 = a / double(m);
+		b1 = b / double(m);
+		x = sqrt(8 / M_E)*(a1 - 0.5) / b1;
+	} while (x*x >= (-4)*(log(b1) / log(M_E)));
+	return x;
+}
 
 void gist()
 {
@@ -69,6 +120,10 @@ void gist()
 	double arrS[n];
 	int number;
 	cin >> number;
+	srand(time(NULL));
+	int x = rand();
+	srand(time(NULL));
+	int y = rand();
 	for (int i = 1; i < n; i++)
 	{
 		switch (number)
@@ -123,6 +178,44 @@ void gist()
 			}
 			break;
 		}
+		case 6:
+		{
+
+			arr[i] = method6(x);
+			x = method1(x);
+			//cout << arr[i] << endl;
+			for (int i = 0; i < n; i++)
+			{
+				arrS[i] = arr[i];
+			}
+
+			break;
+		}
+		case 7:
+		{	double arr1[2];
+		method7(x, y, arr1);
+		arr[i - 1] = arr1[0];
+		arr[i] = arr1[1];
+		x = method1(x);
+		y = method2(y);
+		for (int i = 0; i < n; i++)
+		{
+			arrS[i] = arr[i];
+		}
+
+		}
+		case 8:
+		{
+
+			arr[i] = method8(x, y);
+			x = method1(x);
+			y = method2(y);
+			for (int i = 0; i < n; i++)
+			{
+				arrS[i] = arr[i];
+			}
+		}
+
 		}
 	}
 	int count = 0;
@@ -161,3 +254,4 @@ int main()
 	system("pause");
 	return 0;
 }
+
